c39bdd5b66b0141264158819442ab704910ad2a8
chinmay <chinmaygs82@gmail.com>
2024-03-15 14:16:32 +0530

    On branch addItem  Changes to be committed: 	modified:   src/Components/Home.jsx

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -5,7 +5,13 @@ import {Contextpro} from'../Context'
 
 function Home() {
     const [Data, setData]=useState([])
+    const [Item, setItem]=useState([])
 
+
+function AddItem (data){
+setItem((prevda)=>{return [{id:Math.random().toString(),data:data},...prevda]})
+console.log(Item)
+}
     
     useEffect(() => {
         fetch("https://fakestoreapi.com/products")
@@ -28,7 +34,7 @@ function Home() {
     </div>
    ))}
 </div> 
-{/* <div>
+<div>
     {Item.map((ia)=>(
       <div className="bg-orange-400 object-scale-down rounded-xl shadow-md box-content h-60 w-60 p-4 mx-2 my-2 flex-col text-center " key={ia.id}>
          <img src={ia.data.image} className='object-scale-down overflow-hidden w-24 h-24 rounded-xl mx-auto'/>
@@ -37,7 +43,7 @@ function Home() {
 
       </div>
     ))}
-  </div> */}
+  </div>
 </div>       
    
   )
