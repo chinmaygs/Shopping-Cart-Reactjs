5726c6b0a72bd746249e4169889ebdbced26f098
chinmay <chinmaygs82@gmail.com>
2024-03-17 17:50:11 +0530

    On branch router  Changes to be committed: 	modified:   src/Components/Cart/Cart.jsx

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,7 +1,18 @@
 import React, { useContext } from 'react'
+import { Contextpro } from '../../Context/Context'
+import { MdOutlineRemoveCircle } from "react-icons/md";
+import { MdAddCircle } from "react-icons/md";
+function Cart() {
+  const {Item, dispatch} =useContext(Contextpro)
 
-function Cart(props) {
-const Item=props.port
+  function AddItem (data){
+    dispatch ({type:"ADD",value:data})
+  }
+
+  function Decrease (item){
+    dispatch({type:"DECREASE",value:item})
+  }
+
   return (<div>
     <div className='text-black text-center mt-5'>Your Shopping Cart is empty
     </div>
@@ -11,10 +22,10 @@ const Item=props.port
          <img src={ia.data.image} className='object-scale-down overflow-hidden w-24 h-24 rounded-xl mx-auto'/>
          <h2 className="text-sm font-bold mb-2">{ia.data.title}</h2>
          <h2 className="text-sm font-bold mb-2">{ia.price}</h2>
-         <div className='flex ml-7'>
-         <button onClick={()=>{AddItem(ia.data)}}>+</button>
+         <div className='flex ml-14'>
+         <MdAddCircle className='size-5' onClick={()=>{AddItem(ia.data)}}/>
          <h2 className="text-sm font-bold mb-2">QUANTITY: {ia.quantity}</h2>
-         <button onClick={()=>{Decrease(ia)}}>-</button>
+         <MdOutlineRemoveCircle className='size-5' onClick={()=>{Decrease(ia)}}/>
          </div>
       </div>
       
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,16 +1,12 @@
 import React, { useState } from 'react'
-import { useEffect } from 'react'
+import { useEffect , useReducer} from 'react'
 import { useContext } from 'react'
-import {Contextpro} from'../../Context/Context';
-import Datas from './Datas';
-import Cart from '../Cart/Cart';
+import { Contextpro } from '../../Context/Context'
 
 function Home() {
     const [Data, setData]=useState([])
-  const {Item, dispatch} =useContext(Contextpro)
-function Decrease (item){
-  dispatch({type:"DECREASE",value:item})
-}
+  const {dispatch} =useContext(Contextpro)
+
 
 function AddItem (data){
   dispatch ({type:"ADD",value:data})
@@ -25,8 +21,17 @@ function AddItem (data){
         },[])
   return (
    <div>
-   <div >
-<Datas port={Data}/>
+   <div className='grid grid-cols-4 mx-3 my-3'>
+   {Data.map((da)=>(
+    <div className="bg-orange-400 object-scale-down rounded-xl shadow-md box-content h-60 w-60 p-4 mx-2 my-2 flex-col text-center " key={da.id}>
+      <img src={da.image} className='object-scale-down overflow-hidden w-24 h-24 rounded-xl mx-auto'/>
+  <h2 className="text-sm font-bold mb-2">{da.title}</h2>
+  <p className="text-gray-600">{da.price}</p>
+  <button className="text-blue-500 hover:underline" onClick={()=>{AddItem(da)}}>Add to Cart</button>
+  <br/>
+
+    </div>
+   ))}
 </div> 
 {/* <div>
     {Item.map((ia)=>(
@@ -46,7 +51,6 @@ function AddItem (data){
 <h2 className="text-sm text-black font-bold mb-2">TOTAL:{Item.reduce((total,current)=>{return total+current.price},0)}</h2>
 </div>
   </div> */}
-  {/* <Cart port={Item}/> */}
 </div>       
    
   )
