114d7b71e8bdfad38771233a8d28be2e04ec6f35
chinmay <chinmaygs82@gmail.com>
2024-03-17 18:32:29 +0530

    On branch router  Changes to be committed: 	modified:   src/Components/Cart/Cart.jsx

--- src/Components/Home/Datas.jsx
+++ src/Components/Home/Datas.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
 
 function Datas(props) {
-    
+  
+  const {Data,AddItem}=props
+
   return (
-    <div><div className='className='grid grid-cols-4 mx-3 my-3>
-        {props.port.map((da)=>{
-        <div className="bg-orange-400 object-scale-down rounded-xl shadow-md box-content h-60 w-60 p-4 mx-2 my-2 flex-col text-center " key={da.id}>
-        <img src={da.image} className='object-scale-down overflow-hidden w-24 h-24 rounded-xl mx-auto'/>
-        <h2 className="text-sm font-bold mb-2">{da.title}</h2>
-        <p className="text-gray-600">{da.price}</p>
-        <button className="text-blue-500 hover:underline" onClick={()=>{AddItem(da)}}>Add to Cart</button>
-        <br/>
-        </div>
-         })}</div></div>
+   <div className='grid grid-cols-4 mx-3 my-3'>
+   {Data.map((da)=>(
+    <div className="bg-orange-400 object-scale-down rounded-xl shadow-md box-content h-60 w-60 p-4 mx-2 my-2 flex-col text-center " key={da.id}>
+    <img src={da.image} className='object-scale-down overflow-hidden w-24 h-24 rounded-xl mx-auto'/>
+    <h2 className="text-sm font-bold mb-2">{da.title}</h2>
+    <p className="text-gray-600">{da.price}</p>
+    <button className="text-blue-500 hover:underline" onClick={()=>{AddItem(da)}}>Add to Cart</button>
+    </div>
+    ))}
+    </div>
   )
 }
 
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,9 +2,10 @@ import React, { useState } from 'react'
 import { useEffect , useReducer} from 'react'
 import { useContext } from 'react'
 import { Contextpro } from '../../Context/Context'
+import Datas from './Datas'
 
 function Home() {
-    const [Data, setData]=useState([])
+  const [Data, setData]=useState([])
   const {dispatch} =useContext(Contextpro)
 
 
@@ -21,37 +22,8 @@ function AddItem (data){
         },[])
   return (
    <div>
-   <div className='grid grid-cols-4 mx-3 my-3'>
-   {Data.map((da)=>(
-    <div className="bg-orange-400 object-scale-down rounded-xl shadow-md box-content h-60 w-60 p-4 mx-2 my-2 flex-col text-center " key={da.id}>
-      <img src={da.image} className='object-scale-down overflow-hidden w-24 h-24 rounded-xl mx-auto'/>
-  <h2 className="text-sm font-bold mb-2">{da.title}</h2>
-  <p className="text-gray-600">{da.price}</p>
-  <button className="text-blue-500 hover:underline" onClick={()=>{AddItem(da)}}>Add to Cart</button>
-  <br/>
-
-    </div>
-   ))}
-</div> 
-{/* <div>
-    {Item.map((ia)=>(
-      <div className="bg-orange-400 object-scale-down rounded-xl shadow-md box-content h-60 w-60 p-4 mx-2 my-2 flex-col text-center " key={ia.id}>
-         <img src={ia.data.image} className='object-scale-down overflow-hidden w-24 h-24 rounded-xl mx-auto'/>
-         <h2 className="text-sm font-bold mb-2">{ia.data.title}</h2>
-         <h2 className="text-sm font-bold mb-2">{ia.price}</h2>
-         <div className='flex ml-7'>
-         <button onClick={()=>{AddItem(ia.data)}}>+</button>
-         <h2 className="text-sm font-bold mb-2">QUANTITY: {ia.quantity}</h2>
-         <button onClick={()=>{Decrease(ia)}}>-</button>
-         </div>
-      </div>
-      
-    ))}
-<div>
-<h2 className="text-sm text-black font-bold mb-2">TOTAL:{Item.reduce((total,current)=>{return total+current.price},0)}</h2>
-</div>
-  </div> */}
-</div>       
+    <Datas Data={Data} AddItem={AddItem}/>
+    </div>       
    
   )
 }
